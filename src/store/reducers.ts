cd8848dd83ebdeb69bdf4ccaefbb1457a4a8141b
Tural <tosmanov@interprocom.ru>
2025-03-25 11:10:40 +0300

    Added redux

---
+++ src/store/reducers.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from '@reduxjs/toolkit';
+
+import app from 'features/App/reducer';
+
+export default combineReducers({
+    app,
+})
---
+++ src/store/types.ts
@@ -0,0 +1,13 @@
+import type {
+    ThunkAction,
+    Action,
+    CaseReducer,
+    PayloadAction
+} from '@reduxjs/toolkit';
+
+import store from 'store';
+
+
+export type T_RootState = ReturnType<typeof store.getState>
+export type T_AppThunk<ReturnType = void> = ThunkAction<ReturnType, T_RootState, unknown, Action<string>>
+export type T_Reducer<S, A = any> = CaseReducer<S, PayloadAction<A>>
